884ffc44261492df08f61313c3461f1e77583e61
ranjan-atmegame <ranjan.kumar@atmegame.com>
2023-04-25 10:41:16 +0530

    changed environment to TEST server

--- src/config/index.js
+++ src/config/index.js
@@ -1,1 +1,10 @@
-export const API_URL = process.env.REACT_APP_DEV_API_URL;
+const ENV = process.env.REACT_APP_ENV;
+
+let SERVER = "";
+if (ENV === "TEST") {
+  SERVER = process.env.REACT_APP_TEST_API_URL;
+} else {
+  SERVER = process.env.REACT_APP_DEV_API_URL;
+}
+
+export const API_URL = SERVER;
